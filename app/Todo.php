f1ac40c60ea71951f58d8dcaf6ea30697711c08b
Kaloyan Yosifov <kyosifov@2create.studio>
2020-05-02 20:31:09 +0300

    Add create todo input

--- app/Todo.php
+++ app/Todo.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Todo extends Model
 {
+    protected $fillable = [
+        'name',
+        'text',
+        'user_id',
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
