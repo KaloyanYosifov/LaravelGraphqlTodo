0b7a3324d0987a11e619f8eb458e37b38de76c5e
Kaloyan Yosifov <kyosifov@2create.studio>
2020-05-03 10:52:14 +0300

    Add apollo and create an any match

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', 'BaseController@index')->where('any', '^(?!graphql|graphql-playground).*$');
